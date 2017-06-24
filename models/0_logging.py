b40c79db13bb6d9f3a16eea58cc8599af2929fac
haimavni <haimavni@gmail.com>
2017-06-24 13:24:37 +0300

    typo

--- models/0_logging.py
+++ models/0_logging.py
@@ -1,7 +1,6 @@
 import distutils.dir_util
 
 def log_path():
-    path = 'applications/' + request.application + '/logs/
+    path = 'applications/' + request.application + '/logs/'
     distutils.dir_util.mkpath(path)
     return path
-
