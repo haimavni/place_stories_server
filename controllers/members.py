36e1f2a4ada06a16c1fe6164d9cf35b0ff2981a5
haimavni <haimavni@gmail.com>
2017-07-15 18:01:31 +0300

    bug while creating new member

--- controllers/members.py
+++ controllers/members.py
@@ -37,6 +37,7 @@ def get_member_details(vars):
             ),
             slides=[],
             spouses=[],
+            member_stories = [],
             facePhotoURL = request.application + '/static/images/dummy_face.png'
         )
         return new_member
