708cda592270b3c2254ffd55ae17940e49b959b4
haimavni <haimavni@gmail.com>
2017-07-27 00:32:26 +0300

    wip

--- private/test_mailer.py
+++ private/test_mailer.py
@@ -7,32 +7,32 @@ import time
 
 def main():
 
-     message = MIMEMultipart("alternative")
-     message["Subject"] = "test"
-     message["From"]    = "info@gbstories.org"
-     message["To"]      = "haimavni@gmail.com"
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "test"
+    message["From"]    = "info@gbstories.org"
+    message["To"]      = "haimavni@gmail.com"
 
-     text = "hello world"
+    text = "hello world"
 
-     html = '''<strong>Hellow</strong>world!'''
+    html = '''<strong>Hellow</strong>world!'''
 
-     message.attach(MIMEText(text, "plain"))
-     message.attach(MIMEText(html, "html"))
+    message.attach(MIMEText(text, "plain"))
+    message.attach(MIMEText(html, "html"))
 
-     try:
-         print("send message to {to}".format(
+    try:
+        print("send message to {to}".format(
              to = message["To"]
          ))
-         server = smtplib.SMTP("localhost")
-         server.sendmail(
+        server = smtplib.SMTP("localhost")
+        server.sendmail(
              message["From"],
              message["To"],
              message.as_string()
          )
-         server.quit()
-     except smtplib.SMTPException:
+        server.quit()
+    except smtplib.SMTPException:
         print("e-mail send error")
-     time.sleep(5)
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
