ae9a5693a6b11031c7d9797e72a2136966bf7375
haimavni <haimavni@gmail.com>
2018-04-21 17:59:22 +0300

    chats controller

--- controllers/stories.py
+++ controllers/stories.py
@@ -112,48 +112,6 @@ def upload(vars):
     return dict(success='files-loaded-successfuly')
 
 @serve_json
-def read_chatroom(vars):
-    messages = db(db.TblChats.chat_group==int(vars.room_number)).select()
-    for msg in messages:
-        msg.sender_name = auth.user_name(msg.author)
-        msg.message = msg.message.replace('\n', '<br/>')
-    chatroom_name = db(db.TblChatGroup.id==int(vars.room_number)).select().first().name
-    return dict(chatroom_name=chatroom_name,
-                messages=messages,
-                user_message='')
-
-@serve_json
-def read_chatrooms(vars):
-    lst = db(db.TblChatGroup).select()
-    for rec in lst:
-        rec.user_message = 'bla'
-    dic = dict()
-    for i, rec in enumerate(lst):
-        dic[rec.id] = i
-    return dict(chatrooms=lst, chatroom_index=dic)
-
-@serve_json
-def add_chatroom(vars):
-    chatroom_id = db.TblChatGroup.insert(name=vars.new_chatroom_name,
-                                         moderator_id=auth.current_user())
-    return dict(chatroom_id=chatroom_id)
-
-@serve_json
-def send_message(vars):
-    now = datetime.datetime.now()
-    db.TblChats.insert(chat_group=int(vars.room_number),
-                       author=auth.current_user(),
-                       timestamp=now,
-                       message=vars.user_message)
-    ws_messaging.send_message(key='INCOMING_MESSAGE' + vars.room_number, 
-                              group='CHATROOM' + vars.room_number,
-                              author=auth.current_user(),
-                              timestamp=str(now)[:19],
-                              sender_name=auth.user_name(),
-                              message=vars.user_message.replace('\n', '<br/>'))
-    return dict(good=True)
-
-@serve_json
 def add_face(vars):
     face = vars.face
     face.r = face.r or 20
