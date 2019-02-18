31be7151f80b2989d818ffe92e0bc4d87b7df063
haimavni <haimavni@gmail.com>
2019-02-18 18:13:06 +0200

    fix all date ends applied

--- models/update_fixes.py
+++ models/update_fixes.py
@@ -1,5 +1,6 @@
 import random
 import time
+from date_utils import fix_all_date_ends
 
 def _delay():
     n = random.randint(0, 1000000)
@@ -18,11 +19,13 @@ def _apply_fixes():
         return
     last_fix = sorted(_fixes)[-1]
     last_applied_fix = db(db.TblConfiguration.id==1).select().first().fix_level or 0
-    if last_applied_fix < last_fix:
-        _delay()
-        last_applied_fix = db(db.TblConfiguration.id==1).select().first().fix_level or 0
-        if last_applied_fix >= last_fix:
-            return
+    if last_applied_fix >= last_fix:
+        return
+    _delay()
+    last_applied_fix = db(db.TblConfiguration.id==1).select().first().fix_level or 0
+    if last_applied_fix >= last_fix:
+        return
+    db(db.TblConfiguration.id==1).update(fix_level=last_fix + 1000)        
     for f in sorted(_fixes):
         if f > last_applied_fix:
             _fixes[f]()
@@ -32,7 +35,8 @@ def init_photo_back_sides():
     db(db.TblPhotos.is_back_side==None).update(is_back_side=False)
 
 _fixes = {
-    1: init_photo_back_sides
+    1: init_photo_back_sides,
+    2: fix_all_date_ends
 }
 
 _init_configuration_table()
