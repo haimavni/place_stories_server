86a9f2b799923329d168ac43525f712d8caecf9b
haimavni <haimavni@gmail.com>
2019-04-08 20:59:25 +0300

    term topics

--- controllers/terms.py
+++ controllers/terms.py
@@ -3,31 +3,12 @@ import stories_manager
 from members_support import calc_grouped_selected_options, calc_all_tags, get_tag_ids
 
 @serve_json
-def get_term_list(vars):
-    lst = db((db.TblStories.used_for == STORY4TERM) & \
-             (db.TblStories.deleted != True) & \
-             ###(db.TblTerms.deleted != True) & \
-             (db.TblTerms.story_id == db.TblStories.id)).select(orderby=db.TblStories.name)
-    result = [dict(story_text=rec.TblStories.story,
-                   preview=get_reisha(rec.TblStories.preview, 40),
-                   name=rec.TblStories.name, 
-                   story = get_story_by_id(rec.TblTerms.story_id),
-                   story_id=rec.TblStories.id, 
-                   source=rec.TblStories.source,
-                   id=rec.TblTerms.id) for rec in lst]
-    return dict(term_list=result)
-
-@serve_json
 def delete_term(vars):
     rec = db(db.TblTerms.id == int(vars.term_id)).select().first()
     rec.update(deleted=True)
     story_id = rec.story_id
     db(db.TblStories.id == story_id).update(deleted=True)
     return dict()
-
-def get_story_by_id(story_id):
-    sm = stories_manager.Stories()
-    return sm.get_story(story_id)
 
 @serve_json
 def apply_to_checked_terms(vars):
@@ -65,4 +46,68 @@ def apply_to_checked_terms(vars):
         rec = db(db.TblStories.id==rec.story_id).select().first()
         rec.update_record(keywords=keywords)
     return dict()
+
+@serve_json
+def get_term_list(vars):
+    params = vars.params
+    if params.checked_term_list:
+        lst0 = db(db.TblTerms.story_id.belongs(params.checked_term_list)).select()
+        lst0 = [rec for rec in lst0]
+        for rec in lst0:
+            rec.checked = True
+    else:
+        lst0 = []
+    selected_topics = params.selected_topics or []
+    if selected_topics:
+        lst = get_term_list_with_topics(params)
+    else:
+        q = make_terms_query(params)
+        lst = db(q).select(orderby=~db.TblTerms.id)
+    selected_term_list = params.selected_term_list
+    lst = [rec for rec in lst if rec.story_id not in params.checked_term_list]
+    lst = lst0 + lst
+    term_list = [rec for rec in lst]
+    for rec in term_list:
+        story = get_story_by_id(rec.story_id)
+        rec.story = story
+    return dict(term_list=term_list, no_results=not term_list)
+
+#----------------support functions-----------------
+
+def get_term_list_with_topics(vars):
+    first = True
+    topic_groups = calc_grouped_selected_options(vars.selected_topics)
+    for topic_group in topic_groups:
+        q = make_terms_query(vars) #if we do not regenerate it the query becomes accumulated and necessarily fails
+        q &= (db.TblItemTopics.item_id==db.TblTerms.id) & (db.TblItemTopics.item_type.like('%T%'))
+        ##topic_ids = [t.id for t in topic_group]
+        sign = topic_group[0]
+        topic_group = topic_group[1:]
+        q1 = db.TblItemTopics.topic_id.belongs(topic_group)
+        if sign == 'minus':
+            q1 = ~q1
+        q &= q1
+        lst = db(q).select(orderby=~db.TblTerms.id)
+        lst = [rec.TblTerms for rec in lst]
+        bag1 = set(r.id for r in lst)
+        if first:
+            first = False
+            bag = bag1
+        else:
+            bag &= bag1
+    dic = {}
+    for r in lst:
+        dic[r.id] = r
+    result = [dic[id] for id in bag]
+    return result
+
+def make_terms_query(params):
+    q = (db.TblTerms.deleted!=True)
+    if params.selected_terms:
+        q &= (db.TblTerms.story_id.belongs(params.selected_terms))
+    return q
+
+def get_story_by_id(story_id):
+    sm = stories_manager.Stories()
+    return sm.get_story(story_id)
 
