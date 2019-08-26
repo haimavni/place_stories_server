def44abb3eeed3a1a87e888f1ff5287d8d2b5f41
haimavni <haimavni@gmail.com>
2019-08-26 13:22:57 +0300

    include url in chat activity notification

--- controllers/chats.py
+++ controllers/chats.py
@@ -76,9 +76,11 @@ def notify_chatters(user_id, chatroom_id):
     messages = db(db.TblChats.id==chatroom_id).select() #todo: also ignore very recent ones?
     chatroom = db(db.TblChatGroup.id==chatroom_id).select().first()
     now = datetime.datetime.now()
+    url = ''
     if chatroom.story_id:
         story = db(db.TblStories.id==chatroom.story_id).select().first()
         story.update_record(last_chat_time=now)
+        url = calc_url(story)
         #url by story usage
     hour_ago = now - datetime.timedelta(hours=1)
     msgs = [msg for msg in messages if (msg.timestamp < hour_ago) and (msg.author != user_id)]
@@ -89,7 +91,22 @@ def notify_chatters(user_id, chatroom_id):
     receivers = db(db.auth_user.id.belongs(users)).select(db.auth_user.email)
     receivers = [r.email for r in receivers]
     message = ('', '''
-    New activity in a discussion group
-    ''')
-    mail.send(to=receivers, subject='New activity in chatroom', app=request.application, message=message)
+    New activity in a discussion group of {app}.
+    <br>
+    Click <a href="{host}{url}">here</a> to view the messages.
+    '''.format(host=request.env.http_host.split(':')[0], app=request.application, url=url))
+    mail.send(to=receivers, subject='New activity in chatroom', message=message)
     #todo: collect relevant chatters. email them that a message was added. include link to the relevant page.
+    
+def calc_url(story):
+    if story.used_for == STORY4EVENT:
+        return '/{app}/static/aurelia/index.html#/story-detail/{sid}/*?search_type=simple&what=story'.format(app=request.application, sid=story.id)
+    if story.used_for == STORY4TERM:
+        return '/{app}/static/aurelia/index.html#/term-detail/{sid}/*?what=term'.format(app=request.application, sid=story.id)
+    if story.used_for == STORY4PHOTO:
+        photo = db(db.TblPhotos.story_id==story.id).select().first()
+        pid = photo.id
+        if photo:
+            return '/{app}/static/aurelia/index.html#/photo-detail/{pid}/*'.format(app=request.application, pid=story.id)
+    return ''
+    
