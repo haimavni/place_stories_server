663bcaf229288d70a44293bd80ac644420953e90
haimavni <haimavni@gmail.com>
2019-09-19 01:43:44 +0300

    app specific index. wip.

--- private/update_master.bash
+++ private/update_master.bash
@@ -1,6 +1,6 @@
 cd /home/www-data/tol_server_master
 git pull
-bash ./private/restart_servers.bash
 python private/create_all_app_indexes.py
+bash ./private/restart_servers.bash
 cd /home/www-data/tol_server_master/static/aurelia
 mv -f curr_version.tmp curr_version.txt
