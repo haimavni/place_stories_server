8577860cc8ba91f5de6f0416d8a5324f4186db7c
haimavni <haimavni@gmail.com>
2019-09-19 02:05:02 +0300

    app indexii. wip.

--- private/create_all_app_indexes.py
+++ private/create_all_app_indexes.py
@@ -1,14 +1,14 @@
 import os
 
 def create_all_app_indexes():
-    os.system('echo create all indexii>../logs/create_indexii.log')
+    os.system('echo create all indexii>./logs/create_indexii.log')
     path = '../web2py/applications/'
     apps = os.listdir(path)
     apps = [app for app in apps if os.path.islink(os.path.join(path, app))]
     for app in apps:
         if app in ['welcome', 'tol_hub']:
             continue
-        os.system('python ../../web2py/web2py.py -S {app}/admin/create_app_index>>../logs/create_indexii.log'.format(app=app))
+        os.system('python ../web2py/web2py.py -S {app}/admin/create_app_index>>./logs/create_indexii.log'.format(app=app))
     print 'all app indexes were created'
 
 create_all_app_indexes()
