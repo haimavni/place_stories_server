92bed749b193455d61438f379353cb1a5da84dbc
haimavni <haimavni@gmail.com>
2019-10-02 19:03:15 +0300

    create all indexes for www too

--- private/update_www.bash
+++ private/update_www.bash
@@ -1,5 +1,6 @@
 cd /home/www-data/tol_server_www
 git pull
+python ./private/create_all_app_indexes.py
 bash ./private/restart_servers.bash
 cd /home/www-data/tol_server_www/static/aurelia
 mv -f curr_version.tmp curr_version.txt
