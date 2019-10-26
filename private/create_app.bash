8b1a7826b29a152c2fb8537b26fead839e6c043a
haimavni <haimavni@gmail.com>
2019-10-27 00:32:55 +0300

    create app. wip.

--- private/create_app.bash
+++ private/create_app.bash
@@ -19,9 +19,11 @@ sudo -u postgres createdb $app_name
 cp -a /gb_photos/tolbase /gb_photos/$app_name
 chown -R www-data:www-data /gb_photos/$app_name
 #create link to make the app accessible
-ln --symbolic -T /home/www-data/$server /home/www-data/web2py/applications/$app_name
+cd /home/www-data/web2py/applications
+ln --symbolic -T ../../$server $app_name
 #add the new app to the scheduler
 #to do
 #init the database. create owners account with all privileges
-python /home/www-data/web2py/web2py.py -S $app_name/init_app/init_database$vars
+cd ..
+python web2py.py -S $app_name/init_app/init_database$vars
 
