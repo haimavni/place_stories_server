eb37aea360d498da5673d4db7701064533a0935c
haimavni <haimavni@gmail.com>
2020-11-08 09:40:14 +0200

    store web2py customizations in a tracked file

---
+++ private/make_web2py_customization_dif.bash
@@ -0,0 +1,1 @@
+diff -r --no-dereference /home/haim/web2py_src/web2py/gluon /home/haim/PycharmProjects/web2PyProject/web2py/gluon > /home/haim/PycharmProjects/web2PyProject/tol3/private/web2py-customizations.dif
