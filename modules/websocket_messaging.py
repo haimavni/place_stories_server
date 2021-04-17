7dbfead585237a36fcd3bcc3ed3ac378dbf7f2d9
haimavni <haimavni@gmail.com>
2021-04-17 16:07:19 +0300

    done debugging get story list.

--- modules/websocket_messaging.py
+++ modules/websocket_messaging.py
@@ -106,12 +106,8 @@ def websocket_send(url, message, hmac_key=None, group='default'):
     sig = hmac_key and hmac.new(to_bytes(hmac_key), to_bytes(message), digestmod='MD5').hexdigest() or ''
     params = urlencode(
         {'message': message, 'signature': sig, 'group': group})
-    comment = inject("comment")
-    comment(f"websocket send. url: {url}")
     f = urlopen(url, to_bytes(params))
-    comment("websocket send. after urlopen")
     data = f.read()
-    comment("websocket send. after read")
     f.close()
     return data
 
