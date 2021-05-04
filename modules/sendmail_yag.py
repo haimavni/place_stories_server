7941b0332b9923a7aa916f195d4e3a6a3c7d62c4
haimavni <haimavni@gmail.com>
2021-05-04 20:34:27 +0300

    audios upload. red messages ib server exceptions.

---
+++ modules/sendmail_yag.py
@@ -0,0 +1,16 @@
+import yagmail
+
+to_mail = "spivak4.mark@gmail.com"
+from_mail = "spivak.mark4@gmail.com"
+my_mail = from_mail
+subject = "Yagmail test with attachment"
+body = "Sending mail with Yagmail"
+attachment = "sendmail_yag.py"
+
+yag = yagmail.SMTP(my_mail)
+yag.send(
+    to_mail,
+    subject,
+    body,
+    attachment
+)
