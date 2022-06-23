c0c17bec15f519a7646f84800f886a61265c0b04
haimavni <haimavni@gmail.com>
2022-06-23 20:05:40 +0300

    display web2py errors

---
+++ controllers/errors.py
@@ -0,0 +1,23 @@
+import os
+
+def error_link(err_code):
+    host = request.env.http_host
+    app =  request.application
+    err_url = f'/admin/default/ticket/{app}/{err_code}'
+    return err_url
+
+def show_error_list():
+    app = request.application
+    dir = f'applications/{app}/errors'
+    err_list = os.listdir(dir)
+    err_list = sorted(err_list, reverse=True)
+    result = '<ul>'
+    for err in err_list:
+        url = error_link(err)
+        item = f'<li><a href={url} target="blank">{err}</a></li>'
+        result += item
+    result += '</ul>'
+    return result
+
+   
+   
