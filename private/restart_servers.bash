b840f4cb70dea8dad1013be972ff1ed29417e4ff
haimavni <haimavni@gmail.com>
2022-07-11 00:43:31 +0300

    revive sslmessaging

--- private/restart_servers.bash
+++ private/restart_servers.bash
@@ -3,9 +3,9 @@ chown -R www-data:www-data .
 systemctl stop web2py-scheduler.service
 systemctl stop nginx
 systemctl stop emperor.uwsgi.service
-#systemctl stop sslmessaging.service
+systemctl stop sslmessaging.service
 systemctl restart memcached
-#systemctl start sslmessaging.service
+systemctl start sslmessaging.service
 systemctl start nginx
 systemctl start emperor.uwsgi.service
 systemctl start web2py-scheduler.service
