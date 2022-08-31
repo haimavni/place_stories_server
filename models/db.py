dd3051a13eb8c42f657d6b3387ed2b0ad259c908
haimavni <haimavn@gmail.com>
2022-08-31 16:55:47 +0300

    verify os.getenv works

--- models/db.py
+++ models/db.py
@@ -19,13 +19,14 @@ myconf = AppConfig(reload=True)
 
 
 def __open_db():
-    comment("enter open db")
     db_host = os.getenv('DB_HOST') or "localhost"
     dbname = request.application
     adapter = 'psycopg2:'
     db_user = os.getenv('DB_USER') or "lifestone"
-    db_password = os.getenv('DB_PASSWORD') or "V3geHanu"
+    db_password = os.getenv('DB_PASSWORD')
     comment(f"db_password is {db_password}")
+    if not db_password:
+        db_password = "V3geHanu"
     if not db_password:
         raise Exception("db_password is still missing")
 
@@ -39,7 +40,6 @@ def __open_db():
     except Exception as e:
         comment(f'Failed to open db {db_spec}. Error: {e}.')
         raise
-    comment("exit open db")
     return db
 
 
