d240186b3bcd3ca88fb634e39e116dc9e4cd21ef
haimavni <haimavn@gmail.com>
2022-09-12 11:11:56 +0300

    deployment. wip.

--- private/create_all_app_indexes.py
+++ private/create_all_app_indexes.py
@@ -2,17 +2,24 @@ import os
 import sys
 
 def create_all_app_indexes():
+    web2py_dir = '/home/www-data/py38env/web2py'
     os.system('echo create all indexii>./logs/create_indexii.log')
-    path = '../web2py/applications/'
+    path = f'{web2py_dir}/applications/'
     if len(sys.argv) > 1:
         apps = sys.argv[1:]
     else:
         apps = os.listdir(path)
     apps = [app for app in apps if os.path.islink(os.path.join(path, app))]
+    failed = []
     for app in apps:
-        if app in ['welcome', 'tol_hub', 'gbs']:
+        if app in ['welcome', 'gbs']:
             continue
-        os.system('python ../web2py/web2py.py -S {app}/admin/create_app_index>>./logs/create_indexii.log'.format(app=app))
-    print('all app indexes were created')
+        result = os.system(f'python {web2py_dir}/web2py.py -S {app}/admin/create_app_index>>./logs/create_indexii.log')
+        if result:
+            failed.append(app)
+    if failed:
+        print(f'apps that failed to create app index: {failed}')
+    else:
+        print('all app indexes were created')
 
 create_all_app_indexes()
