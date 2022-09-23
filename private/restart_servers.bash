f7f7290d7feee89c2b4642150cfb03c8fd3d492e
haimavni <haimavn@gmail.com>
2022-09-24 00:00:10 +0300

    sslmessaging-tolife too!

--- private/restart_servers.bash
+++ private/restart_servers.bash
@@ -4,9 +4,10 @@ systemctl stop web2py-scheduler.service
 systemctl stop nginx
 systemctl stop emperor.uwsgi.service
 systemctl stop sslmessaging.service
+systemctl stop sslmessaging-tolife.service
 #systemctl restart memcached
+systemctl start sslmessaging-tolife.service
 systemctl start sslmessaging.service
 systemctl start nginx
 systemctl start emperor.uwsgi.service
 systemctl start web2py-scheduler.service
-
