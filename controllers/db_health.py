c9b9c3e9e32f853775cdae532b7d8a19bd0c4b87
haimavni <haimavn@gmail.com>
2022-09-28 21:05:25 +0300

    db_health

---
+++ controllers/db_health.py
@@ -0,0 +1,9 @@
+def remove_detached_member_stories():
+    lst = db(db.TblStories.used_for=='STORY4MEMBER').select(db.TblStories.id)
+    lst - [rec.id for rec in lst]
+    detached = 0
+    for sid in lst:
+        if db(db.TblMembers.story_id==sid).count() == 0:
+            comment(f'story #{sid} is detached')
+            detached += 1
+    return f'{detached} detached stories found'
