db7d463dca89f580353025f953c7bbe1d4139f5a
haimavni <haimavn@gmail.com>
2022-09-28 21:09:02 +0300

    typo

--- controllers/db_health.py
+++ controllers/db_health.py
@@ -1,6 +1,6 @@
 def remove_detached_member_stories():
     lst = db(db.TblStories.used_for=='STORY4MEMBER').select(db.TblStories.id)
-    lst - [rec.id for rec in lst]
+    lst = [rec.id for rec in lst]
     detached = 0
     for sid in lst:
         if db(db.TblMembers.story_id==sid).count() == 0:
