07e1cb54dfe2e27916a3f5389ef614641df753e8
haimavni <haimavn@gmail.com>
2022-09-30 15:51:08 +0300

    push state wip

---
+++ controllers/aurelia.py
@@ -0,0 +1,17 @@
+import os
+
+def index():
+    host = request.env.http_host
+    app = request.application
+    lang = 'he'
+    app_config = db(db.TblConfiguration).select().first()
+    app_name_rec = db(
+        (db.TblLocaleCustomizations.lang == lang) & (db.TblLocaleCustomizations.key == 'app-title')).select().first()
+    app_name = app_name_rec.value if app_name_rec else 'Noname' 
+    folder = f'applications/{app}/static/aurelia/script'
+    lst = os.listdir(folder)
+    for fname in lst:
+        if fname.endswith('.js'):
+            if fname.startswith('vendor-bundle'):
+                vendor_bundle = f'https://{host}/{app}/static/aurelia/scripts/{fname}'
+    return dict(app=app, app_name=app_name, vendor_bundle=vendor_bundle)
