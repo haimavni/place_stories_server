dde63bef6fad6e9a2e64fd220271f5da66ce7ab1
haimavni <haimavn@gmail.com>
2022-10-07 09:44:13 +0300

    from push-state to master

---
+++ controllers/aurelia.py
@@ -0,0 +1,22 @@
+import os
+
+def index():
+    curdir = os.getcwd()
+    host = request.env.http_host
+    app = request.application
+    lang = 'he'
+    app_config = db(db.TblConfiguration).select().first()
+    app_name_rec = db(
+        (db.TblLocaleCustomizations.lang == lang) & (db.TblLocaleCustomizations.key == 'app-title')).select().first()
+    app_name = app_name_rec.value if app_name_rec else 'Noname' 
+    folder = f'applications/{app}/static/aurelia/scripts'
+    #return f'folder is {folder}. app_name is {app_name}'
+    lst = os.listdir(folder)
+    vendor_bundle = None
+    for fname in lst:
+        if fname.endswith('.js'):
+            if fname.startswith('vendor-bundle'):
+                vendor_bundle = f'https://{host}/{app}/static/aurelia/scripts/{fname}'
+    if not vendor_bundle:
+        raise Exception("Vendor bundle not found")
+    return dict(app=app, app_name=app_name, vendor_bundle=vendor_bundle)
