b9f1a8846324512bd60f638479d274d5f7a3f71a
haimavni <haimavn@gmail.com>
2022-10-14 21:57:22 +0300

    wip

--- modules/create_card.py
+++ modules/create_card.py
@@ -7,6 +7,8 @@ def card_data(url, img_src, title, description):
             <meta property="og:title"              content="{title}" />
             <meta property="og:description"        content="{description}" />
             <meta property="og:image"              content="{img_src}" />
+            <meta property="og:image:secure_url"   content="{img_src}" />
+            <meta property="og:image:alt"          content="{img_src}" />
             <meta property="og:image:type"         content="image/jpeg" />
             <meta property="og:image:width"        content="800" />
             <meta property="og:image:height"       content="420" />
