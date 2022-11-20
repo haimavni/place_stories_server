65f0095c0d3561630a12a8248fb5d4fa22d6ea55
haimavni <haimavn@gmail.com>
2022-11-20 14:51:33 +0200

    wip

--- controllers/docs.py
+++ controllers/docs.py
@@ -250,6 +250,7 @@ def update_doc_members(vars):
 
 @serve_json
 def update_story_preview(vars):
+    comment(f"update story preview vars: {vars}")
     story_id = int(vars.story_id)
     story_about_id = int(vars.story_about_id)
     story_rec = db(db.TblStories.id==story_id).select().first()
