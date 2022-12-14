cfc6e575ab86d858bfe2f0077d570c7152d97d41
haimavni <haimavn@gmail.com>
2022-12-15 00:04:03 +0200

    wip

--- modules/video_support.py
+++ modules/video_support.py
@@ -14,7 +14,7 @@ def youtube_info(src):
         comment(f"ydl extract info of {url} got exception {e}")
         return None
     try:
-        comment("still alive")
+        comment(f"still alive. thumbnails in yt? {thumbnails in yt} ")
         thumbnails = yt['thumbnails']
         comment(f"thumbnails: {thumbnails}")
         try:
