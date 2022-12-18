c268f4b171a703b64ae2efbc16fcce7fd0bebb10
haimavni <haimavn@gmail.com>
2022-12-18 21:46:46 +0200

    wip

--- controllers/sse.py
+++ controllers/sse.py
@@ -1,6 +1,8 @@
 from sse import publisher
+import time
 
 def subscribe():
+    time.sleep(2000)
     channel = request.vars.channel or "default channel"
     comment(f"entered subscribe. {channel}")
     publisher.subscribe(channel)
