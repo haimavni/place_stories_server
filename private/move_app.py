e11998072250c2807cd7953f555be38048781a5e
haimavni <haimavn@gmail.com>
2022-12-30 14:29:31 +0200

    move_app utility revised

--- private/move_app.py
+++ private/move_app.py
@@ -8,22 +8,53 @@ def db_id(app):
     uri = uri.encode('utf-8')
     return md5(uri).hexdigest()
     
-def move_app(cp_or_mv, app=None, src=None, dst=None):
+def move_app(cp_or_mv, app=None, new_app=None, src=None, dst=None):
     #collect all matching files and move them from src to dst
     #/home/haim/aurelia-gbs/server/tol_server
     if not app:
         app = input("Enter app name: ")
     if not src:
-        src = input("Enter source: ")
+        print("select source:")
+        print("1 - www")
+        print("2 - test")
+        print("3 - master")
+        print("4 - local")
+        option = input("Enter option: ")
+        if option == "4":
+            src = "/home/haim/sandbox/src"
+        else:
+            src = "/home/www-data/tol_" + "www" if option == "1" else "test" if option == "2" else "master"
+        print(f"source is {src}")
     if not dst:
-        dst = input("Enter destination: ")
+        print("select destination:")
+        print("1 - www")
+        print("2 - test")
+        print("3 - master")
+        print("4 - local")
+        option = input("Enter option: ")
+        if option == "4":
+            dst = "/home/haim/sandbox/dst"
+        else:
+            dst = "/home/www-data/tol_" + "www" if option == "1" else "test" if option == "2" else "master"
+        print(f"destination is {src}")
     app_id = db_id(app)
+    target_app = input(f"enter target app ({app}): ")
+    if not target_app:
+        if src == dst:
+            print("copy into itself!")
+            exit()
+        target_app = ""
     path = src + '/databases'
     file_list = listdir(path)
+    n = len(app_id)
+    target_app_id = db_id(target_app)
     file_list = [f for f in file_list if app_id in f]
     with open("/home/haim/move_app.bash", "w") as out:
         for src in file_list:
-            out.write(f"{cp_or_mv} {path}/{src} {dst}\n")
+            if target_app:
+                out.write(f"{cp_or_mv} {path}/{src:64} {dst}/databases/{target_app_id + src[n:]}\n")
+            else:
+                out.write(f"{cp_or_mv} {path}/{src:64} {dst}/\n")
     
 def remove_obsolete_dbs(active_apps=None):
     active_ids = set([])
@@ -59,9 +90,12 @@ if __name__ == '__main__':
             app_name = input("Enter app name: ")
             print("The db id is ", db_id(app_name))
         elif option == "2":
-            cp_or_mv = input("Enter cp or mv: ")
+            cp_or_mv = input("Enter cp or mv (cp): ")
+            if not cp_or_mv:
+                cp_or_mv = "cp"
             if cp_or_mv not in ("mv", "cp"):
                 print("Please enter cp or mv")
+                exit()
             move_app(cp_or_mv)
         elif option == "3":
             remove_obsolete_dbs()
