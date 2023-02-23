b688a7f5559911d7fc83d5a37078806e8e8de5f9
haimavni <haimavn@gmail.com>
2023-02-23 15:59:22 +0200

    wip

--- controllers/videos.py
+++ controllers/videos.py
@@ -333,8 +333,11 @@ def update_video_members(vars):
 def update_cue_members(vars):
     video_id = int(vars.video_id)
     cid = int(vars.cue_id)
+    comment(f"cid is {cid}")
     if not cid:
+        comment("no cid yet")
         cid = db.TblVideoCuePoints.insert(time=vars.time, description=vars.description, member_ids=vars.member_ids)
+        comment(f"cid created: {cid}")
     member_ids = vars.member_ids
     old_member_ids = calc_cue_members(video_id, cid)
     q = (db.TblVideoCuePoints.video_id == video_id) & \
