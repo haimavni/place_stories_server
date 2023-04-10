8faf8f5bebe9aa406770b9204194d911e1435678
haimavni <haimavn@gmail.com>
2023-04-10 19:18:43 +0300

    bash for fixing column names

--- private/fix_column_case.bash
+++ private/fix_column_case.bash
@@ -1,1 +1,20 @@
-sudo -u postgres psql $1 -f /home/www-data/tol_master/private/fix_column_case.sql
+#!/bin/bash
+
+echo fix column names
+
+dbname=$1
+
+if [ -z "$dbname" ]
+then
+    dbname="gbs__master"
+fi
+echo fix column names for $dbname  
+
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+sudo -u postgres psql $dbname -f /home/www-data/tol_master/private/fix_column_case.sql
