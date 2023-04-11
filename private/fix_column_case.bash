b6e8dec1db437fea641bdb6b88224ffee23f4d56
haimavni <haimavn@gmail.com>
2023-04-11 17:49:27 +0300

    bash script for columns rename

--- private/fix_column_case.bash
+++ private/fix_column_case.bash
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo fix column names
+echo Fix column names
+echo ----------------
+echo #
+# declare -a dbnames=("crossing" "gbs__test" "k_aza" "kavigdor" "rishpon")
+declare -a dbnames=("crossing" "k_aza" "ron" "erez_h")
 
-dbname=$1
+for dbname in "${dbnames[@]}"
+do
+    echo fix column names for $dbname
+    read -p "Are you sure? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        sudo -u postgres psql $dbname < /home/www-data/tol_master/private/fix_column_case.sql 
+    fi
+    echo #
+done
 
-if [ -z "$dbname" ]
-then
-    dbname="gbs__master"
-fi
-echo fix column names for $dbname  
 
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-
-sudo -u postgres psql $dbname -f /home/www-data/tol_master/private/fix_column_case.sql
