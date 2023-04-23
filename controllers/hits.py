884fe1d26e45c2e83259fe14a69dea6e9194574d
haimavni <haimavn@gmail.com>
2023-04-23 19:18:13 +0300

    wip

--- controllers/hits.py
+++ controllers/hits.py
@@ -1,18 +1,21 @@
 import datetime
 
+
 @serve_json
 def count_hit(vars):
     what = vars.what.upper()
     date = datetime.datetime.today()
     item_id = int(vars.item_id)
     rec = db(
-        (db.TblPageHits.what == what) & 
+        (db.TblPageHits.what == what) &
         (db.TblPageHits.date == date) &
         (db.TblPageHits.item_id == item_id)).select().first()
     if rec:
-        rec.update_record(count=rec.count + 1, new_count=(rec.new_count or 0) + 1)
+        rec.update_record(count=rec.count + 1,
+                          new_count=(rec.new_count or 0) + 1)
     else:
-        db.TblPageHits.insert(what=what, item_id=item_id, date=date, count=1, new_count=1)
+        db.TblPageHits.insert(what=what, item_id=item_id,
+                              date=date, count=1, new_count=1)
     return dict()
 
 
@@ -50,15 +53,20 @@ def get_hit_statistics(vars):
             q &= (db.TblPageHits.what == what)
             prec = db(q).select(db.TblPageHits.count.sum()).first()
             totals[period] = prec._extra['SUM("TblPageHits"."count")']
-            #totals = [dict(what=rec.what, sum=rec._extra['SUM("TblPageHits", "count")']) for rec in totals]
             if not tbl:
                 continue
-            q &= (db.TblPageHits.item_id==tbl.id)
+            q &= (db.TblPageHits.item_id == tbl.id)
             q &= (tbl.deleted != True)
-            detailed[period] = db(q).select(db.TblPageHits.item_id, tbl.name, db.TblPageHits.count.sum(),
-                                    groupby=[db.TblPageHits.item_id, tbl.name],
-                                    orderby=~db.TblPageHits.count.sum())
-            #detailed = [dict(what=rec.what, item_id=rec.item_id, name=item.name, sum=rec._extra['SUM("TblPageHits", "count")]']) for rec in detailed]
-            #detailed = [dict(what=rec.what, item_id=rec.item_id, sum=rec._extra['SUM("TblPageHits", "count")]']) for rec in detailed]
+            precs = db(q).select(db.TblPageHits.item_id, tbl.name, db.TblPageHits.count.sum(),
+                                 groupby=[db.TblPageHits.item_id, tbl.name],
+                                 orderby=~db.TblPageHits.count.sum())
+            detailed[period] = [parse(prec, tbl.name) for prec in precs]
         result[what] = dict(totals=totals, detailed=detailed)
     return result
+
+
+def parse(prec, tbl_name):
+    return dict(count=prec._extra['SUM("TblPageHits"."count")'],
+                name=prec[tbl_name].name,
+                item_id=prec.TblPageHits.item_id
+                )
