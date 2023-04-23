fef104079356dea7e8e14f71171f896d9963de41
haimavni <haimavn@gmail.com>
2023-04-23 19:05:30 +0300

    wip

--- controllers/hits.py
+++ controllers/hits.py
@@ -48,7 +48,8 @@ def get_hit_statistics(vars):
                 q = db.TblPageHits.id > 0
             q &= (db.TblPageHits.count != None)
             q &= (db.TblPageHits.what == what)
-            totals[period] = db(q).select(db.TblPageHits.count.sum()).first()
+            prec = db(q).select(db.TblPageHits.count.sum()).first()
+            totals[period] = prec._extra['SUM("TblPageHits"."count")']
             #totals = [dict(what=rec.what, sum=rec._extra['SUM("TblPageHits", "count")']) for rec in totals]
             if not tbl:
                 continue
