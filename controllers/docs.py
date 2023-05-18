af4c63aff5a7177ab4e4400a5e3f98f7b9cdad75
haimavni <haimavn@gmail.com>
2023-05-18 22:54:08 +0300

    wip

--- controllers/docs.py
+++ controllers/docs.py
@@ -473,10 +473,11 @@ def get_story_by_id(story_id):
 
 @serve_json
 def upload_thumbnail(vars):
-    x = dir(vars)
+    x = dir(vars.keys())
     comment(f"vars in upload thumbnail: {x}")
-    doc_id = vars.info.doc_id
-    segment_id = vars.info.setment_id
+    info = vars.file.info
+    doc_id = info.doc_id
+    segment_id = info.setment_id
     comment(f"start handling uploaded doc thumbnail file {doc_id}/{segment_id}")
     fil = vars.file
     result = save_uploaded_thumbnail(fil.BINvalue, doc_id, segment_id)
