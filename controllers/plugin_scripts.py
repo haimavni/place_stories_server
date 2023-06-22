c4f1f067521928166397753d5af4c2c47e8a6ee5
haimavni <haimavn@gmail.com>
2023-06-22 12:40:33 +0300

    wip

--- controllers/plugin_scripts.py
+++ controllers/plugin_scripts.py
@@ -1,5 +1,6 @@
 # ---script for developer to evaluate python expressions in the environment
 import datetime
+import inspect
 
 @serve_json
 def load_script(vars):
@@ -25,7 +26,9 @@ def evaluate_script(vars):
         exec(ccode, environment)
         dic = dict()
         for v in environment:
-            # a = environment[v]
+            a = environment[v]
+            if inspect.isfunction(a):
+                continue
             # if callable(a):
             #     continue
             if v not in old_environment or environment[v] != old_environment[v]:
