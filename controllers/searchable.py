d7e1076df9ed8b0e3b1208be0c408144150e7e9d
haimavni <haimavn@gmail.com>
2023-07-06 18:12:28 +0300

    searchable

--- controllers/searchable.py
+++ controllers/searchable.py
@@ -6,10 +6,12 @@ def index():
     if '8000' in host:
         host = "gbstories.org"
         url_base = 'http://{host}/{app}/static/aurelia/index.html#/'.format(host=host,app=app)
+    ubase = f"https://{host}/{app}/"
     
     m_list = db((db.TblMembers.story_id==db.TblStories.id) & (db.TblStories.deleted!=True)).select(db.TblStories.name, db.TblMembers.id, limitby=(0,20))
-    member_list = [A(r.TblStories.name, _href=url_base + "member-details/{}/*".format(r.TblMembers.id)) for r in m_list]
-    
+    # member_list = [A(r.TblStories.name, _href=url_base + "member-details/{}/*".format(r.TblMembers.id)) for r in m_list]
+    member_list = [A(r.TblStories.name, _href=ubase + f"searchable/member/{r.TblMembers.id}") for r in m_list]
+
     p_list = db((db.TblPhotos.story_id==db.TblStories.id) & (db.TblStories.deleted!=True)).select(db.TblStories.name, db.TblPhotos.id, limitby=(0,20))
     photo_list = [A(r.TblStories.name, _href=url_base + "photos/{}/*".format(r.TblPhotos.id)) for r in p_list]
     
@@ -20,3 +22,23 @@ def index():
     term_list = [A(r.name, _href=url_base + "term-detail/{}/*?what=term".format(r.id)) for r in t_list]
     
     return dict(member_list=member_list, photo_list=photo_list, story_list=story_list, term_list=term_list)
+
+def member():
+    member_id = request.args[0]
+    member = db(db.TblMembers.id==member_id).select().first()
+    name = (member.first_name or "") + " " + (member.last_name or "")
+    story_id = member.story_id
+    if story_id:
+        story_rec = db(db.TblStories.id==story_id).select().first()
+        bio = story_rec.story
+    else:
+        bio = ""
+    href = calc_href("member-details", member_id)
+    return dict(name=name, bio=bio,href=href)
+
+def calc_href(what, id):
+    host = request.env.http_host
+    app = request.application
+    return f'https://{host}/{app}/aurelia#/{what}/{id}'
+
+    
