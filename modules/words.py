bb93416d0d9f7445bd608215c473550a8f3f23bb
haimavni <haimavn@gmail.com>
2024-04-17 17:51:56 +0300

    wip

--- modules/words.py
+++ modules/words.py
@@ -253,6 +253,8 @@ def log_available_memory(txt):
 
 def read_words_index():
     db = inject('db')
+    if db(db.TblWordStories).count() == 0:
+        return []
     lst = None
     cmd = """
             SELECT "TblWords"."id", "TblWords"."word", array_agg("TblWordStories"."story_id"), sum("TblWordStories"."word_count")
@@ -261,7 +263,7 @@ def read_words_index():
             GROUP BY "TblWords"."word", "TblWords"."id";
         """
     log_available_memory('before words index query')
-    lst = db.executesql(cmd)
+    # lst = db.executesql(cmd)
     log_available_memory('after words index query')
     lst = sorted(lst, key=lambda item: item[1], reverse=False)
 
